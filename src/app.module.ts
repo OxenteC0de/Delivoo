c88106e11bd67d08c6692a7cd109c72bd9bc45e6
Janaína Bezerra <janainabezerra.ti@gmail.com>
2025-10-08 10:25:33 -0300

    categoria entity, controller e service

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       host:'localhost',
       port: 3306,
       username: 'root',
-      password: 'root',
+      password: '*@5.!6uVbC07ml$',
       database: 'db_delivoo',
       entities:[],
       synchronize: true,
